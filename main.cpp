fa6fd8bebb8a5a3249913f78a015f3a37f205c48
oliveeah <olliebennettx@gmail.com>
2026-06-29 21:18:14 +0100

    template testing T...

--- main.cpp
+++ main.cpp
@@ -1,52 +1,12 @@
-#include <iostream>
-#include <string_view>
-enum Direction { North, East, South, West };
+#include<iostream>
 
-constexpr Direction& CalcNewDirection(Direction& dir)
-{
-    switch (dir)
-    {
-        case North: dir = East; break;
-        case East: dir = South; break;
-        case South: dir = West; break;
-        case West: dir = North; break;
-    }
-    return dir;
-}
+template<typename T>
+void foo(T...) {std::cout << 'A';}
 
-constexpr std::string_view DirectionToString(Direction dir)
-{
-    switch (dir)
-    {
-        case North: return "North";
-        case East: return "East";
-        case South: return "South";
-        case West: return "West";
-    }
-    
-}
+template<typename... T>
+void foo(T...) {std::cout << 'B';}
 
-std::ostream& operator<<(std::ostream& out, Direction dir)
-{
-    out << DirectionToString(dir);
-    return out;  
-}
-
-Direction& operator++(Direction& dir)
-{
-    std::cout << "Current direction: " << dir << '\n';
-    return CalcNewDirection(dir);
-}
-
-int main ()
-{        
-    Direction dir { North };
-
-    for (int i {}; i < 4; ++i)
-    {
-        ++dir;
-        std::cout << "New direction: " << dir << '\n';
-    }
-
-    return 0;
+int main(){
+    foo(1);
+    foo(1,2);
 }
