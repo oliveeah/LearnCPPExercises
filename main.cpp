0e7a2bcb9a79cd8104bbc1ad3d0296ef9204d33b
oliveeah <olliebennettx@gmail.com>
2026-07-03 21:18:03 +0100

    struct tests

--- main.cpp
+++ main.cpp
@@ -1,12 +1,53 @@
 #include<iostream>
 
-template<typename T>
-void foo(T...) {std::cout << 'A';}
+struct Monster 
+{
+    enum MonsterType
+    {
+        ogre,
+		dragon,
+		orc,
+		giant_spider,
+		slime,
+    };
 
-template<typename... T>
-void foo(T...) {std::cout << 'B';}
+    const MonsterType type {};
+    const std::string name {};
+    int health {};
+};
+
+constexpr std::string_view getMonsterType (Monster::MonsterType type)
+{
+	switch (type)
+	{
+	case Monster::ogre:          return "Ogre";
+	case Monster::dragon:        return "Dragon";
+	case Monster::orc:           return "Orc";
+	case Monster::giant_spider:  return "Giant Spider";
+	case Monster::slime:         return "Slime";
+	}
+
+	return "Unknown";
+} 
+
+constexpr void PrintMonsterInfo(const Monster& monster)
+{
+    const std::string_view sv {monster.name};
+
+    std::cout << "Monster Name: " << sv <<
+     " Monster HP: " << monster.health <<
+     " Monster Type: " << getMonsterType(monster.type) << '\n';
+
+}
+
 
 int main(){
-    foo(1);
-    foo(1,2);
+
+	Monster ogre{ Monster::ogre, "Torg", 145 };
+	Monster slime{ Monster::slime, "Blurp", 23 };    
+
+    PrintMonsterInfo(ogre);
+	PrintMonsterInfo(slime);
+
+    return 0;
 }
