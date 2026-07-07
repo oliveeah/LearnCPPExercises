9907c910949988104f48f93ed98984f265d36cd2
oliveeah <olliebennettx@gmail.com>
2026-07-07 17:29:29 +0100

    testing constructors

--- main.cpp
+++ main.cpp
@@ -1,41 +1,29 @@
 #include <iostream>
 
-class Point3D
+class Ball
 {
-    private:
-    int m_x {};
-    int m_y {};
-    int m_z {};
-
     public:
-    auto print() const -> void
-    {
-        std::cout << "x: " << m_x
-        << " y: " << m_y
-        << " z: " << m_z << '\n';
-    }
+    Ball(std::string_view color, double radius)
+    : m_color{color}
+    , m_radius{radius}
+    {}
 
-    auto setValues(int x, int y, int z) -> void
-    {
-        m_x = x;
-        m_y = y;
-        m_z = z;
-    }
+    auto getColor() const -> const std::string& {return m_color;}
+    auto getRadius() const -> double {return m_radius;}
 
-    auto isEqual(const Point3D& pointPassed) const -> const bool 
-    {
-        return (pointPassed.m_x == m_x) && (pointPassed.m_y == m_y) && (pointPassed.m_z == m_z);
-    }
+    private:
+    std::string m_color {};
+    double m_radius {};
 };
 
+auto printBall(const Ball& ball) -> void
+{
+    std::cout << "Ball(" << ball.getColor() << ", " << ball.getRadius() << ")\n";
+}
+
 int main(){
-    Point3D point;
-    point.setValues(1, 2, 3);
 
-    Point3D point2;
-    point2.setValues(4, 5, 6);
-
-    point2.isEqual(point) ? std::cout << "Points are equal\n" : std::cout << "Points are not equal\n";
-
+    Ball ball1{"red", 5.0};
+    printBall(ball1);
     return 0;
 }
