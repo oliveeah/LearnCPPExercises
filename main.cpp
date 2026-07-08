f4f0fd55df18953cf047fe6b4efa74a6b3a72b28
oliveeah <olliebennettx@gmail.com>
2026-07-08 12:24:22 +0100

    constructor delegation

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <string>
+#include <string_view>
 
 class Ball
-{
+{    
+    private:
+    static constexpr std::string_view m_default_color {"black"};
+    static constexpr double m_default_radius {10.0};
+
+    std::string m_color;
+    double m_radius;
+
     public:
-    Ball(std::string_view color, double radius)
+    Ball(double radius)
+    : Ball{ m_default_color, radius }
+    {}
+
+    Ball(std::string_view color = m_default_color, double radius= m_default_radius)
     : m_color{color}
     , m_radius{radius}
     {}
@@ -11,9 +24,6 @@ class Ball
     auto getColor() const -> const std::string& {return m_color;}
     auto getRadius() const -> double {return m_radius;}
 
-    private:
-    std::string m_color {};
-    double m_radius {};
 };
 
 auto printBall(const Ball& ball) -> void
@@ -23,7 +33,13 @@ auto printBall(const Ball& ball) -> void
 
 int main(){
 
-    Ball ball1{"red", 5.0};
-    printBall(ball1);
+    Ball def{};
+    Ball blue{ "blue" };
+    Ball twenty{ 20.0 };
+    Ball blueTwenty{ "blue", 20.0 };
+    printBall(def);
+    printBall(blue);
+    printBall(twenty);
+    printBall(blueTwenty);
     return 0;
 }
