35a18647184a6a21acd46b6f28281a1e95ef4104
oliveeah <olliebennettx@gmail.com>
2026-07-19 18:15:59 +0100

    std::size() and .at()

--- main.cpp
+++ main.cpp
@@ -4,13 +4,9 @@
 
 int main()
 {
-    std::vector<int> results (3);
-
-    std::cin >> results[0] >> results[1] >> results[2];
-
-    std::cout << results[0] << results[1] << results[2];
-    std::cout << results[0]+results[1]+results[2];
-    std::cout << results[0]*results[1]*results[2];
+    std::vector<int> vec = {1, 2, 3, 4, 5};
+    std::cout << "vec size: " << std::size(vec) << '\n';
+    std::cout << "vec at 1: " << vec.at(1) << '\n';
 
     return 0;
 }
