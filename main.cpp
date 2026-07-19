1ad5ea6adb0c7d4042bd3401aa50d4ccfcf24357
oliveeah <olliebennettx@gmail.com>
2026-07-19 16:37:03 +0100

    testing std::vector first time

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <vector>
+
 
 int main()
 {
+    std::vector<int> results (3);
 
-    
+    std::cin >> results[0] >> results[1] >> results[2];
+
+    std::cout << results[0] << results[1] << results[2];
+    std::cout << results[0]+results[1]+results[2];
+    std::cout << results[0]*results[1]*results[2];
+
     return 0;
 }
