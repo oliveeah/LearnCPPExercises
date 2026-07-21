5ed9b615e9f9fa5b6c808021c6e6cc36c1cd447f
oliveeah <olliebennettx@gmail.com>
2026-07-21 13:14:15 +0100

    caching vars when iterating

--- main.cpp
+++ main.cpp
@@ -5,7 +5,10 @@ int main()
 {
     std::vector arr{ 4, 6, 7, 3, 8, 2, 1, 9 };
     std::cout << "|";
-    for(std::size_t i {}; i < arr.size(); ++i)
+
+    auto i {0uz};
+    const auto size { arr.size() };
+    for(; i < size; ++i)
     {
         std::cout << arr[i] << "|";
     }
