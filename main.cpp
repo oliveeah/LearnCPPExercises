82afeda586e4c6382054c1e43925d74380f4b5b6
oliveeah <olliebennettx@gmail.com>
2026-07-21 13:21:27 +0100

    adding template function, changing compiler to clang + LLVM tho

--- main.cpp
+++ main.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 #include <vector>
 
+template <typename T>
+auto printArray(const std::vector<T>& arr) -> void
+{
+    static const auto size { arr.size() };
+    for (auto i {0uz}; i < size; ++i)
+    {
+        std::cout << arr[i] << " ";
+    }
+}
+
 int main()
 {
     std::vector arr{ 4, 6, 7, 3, 8, 2, 1, 9 };
-    std::cout << "|";
 
-    auto i {0uz};
-    const auto size { arr.size() };
-    for(; i < size; ++i)
-    {
-        std::cout << arr[i] << "|";
-    }
+    printArray(arr); // use function template to print array
 
     return 0;
 }
