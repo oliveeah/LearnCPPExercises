a7908989ad238ba89ace194395fcf668b6d9f672
oliveeah <olliebennettx@gmail.com>
2026-07-22 17:35:41 +0100

    resolved compiler issues and corrupted extension

--- main.cpp
+++ main.cpp
@@ -1,18 +1,39 @@
 #include <iostream>
 #include <vector>
 #include <string_view>
+#include <cassert>
 
-class FizzBuzz {
-    private:
-    inline static const std::vector<int> m_fizzbuzz_divisors { 3, 5, 7, 11, 13, 17, 19 };
-    inline static const std::vector<std::string_view> fizzbuzz_words { "fizz", "buzz", "pop", "bang", "jazz", "pow", "boom" };
+auto FizzBuzz(int count) -> void
+{
+    const std::vector<int> divisors{ 3, 5, 7, 11, 13, 17, 19 };
+    const std::vector<std::string_view> words{ "fizz", "buzz", "pop", "bang", "jazz", "pow", "boom" };
 
-    public:
-    FizzBuzz() = default;
-    ~FizzBuzz() = default;
+    assert(divisors.size() == words.size());
+    const auto size = divisors.size();
 
-};
+    for (int i { 1 }; i < count; ++i) //foreach i->count
+    {
+        bool printed { false };
+        for (auto j {0uz}; j < size; ++j)
+        {
+            if (i % divisors[j] == 0)
+            {
+                std::cout << words[j];
+                printed = true;
+            }
+        }
+        if (!printed)
+        {
+            std::cout << i;
+        }
+        std::cout << '\n';
+    }
+
+}
+
+
 
 int main() {
+    FizzBuzz(150);
     return 0;
 }
