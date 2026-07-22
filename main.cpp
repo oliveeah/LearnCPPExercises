2a5c0d040a4e8b2aa966a150623855aedf007807
oliveeah <olliebennettx@gmail.com>
2026-07-22 18:46:54 +0100

    learncpp range based for loop

--- main.cpp
+++ main.cpp
@@ -1,39 +1,22 @@
 #include <iostream>
-#include <vector>
+#include <string>
 #include <string_view>
-#include <cassert>
-
-auto FizzBuzz(int count) -> void
-{
-    const std::vector<int> divisors{ 3, 5, 7, 11, 13, 17, 19 };
-    const std::vector<std::string_view> words{ "fizz", "buzz", "pop", "bang", "jazz", "pow", "boom" };
-
-    assert(divisors.size() == words.size());
-    const auto size = divisors.size();
-
-    for (int i { 1 }; i < count; ++i) //foreach i->count
-    {
-        bool printed { false };
-        for (auto j {0uz}; j < size; ++j)
-        {
-            if (i % divisors[j] == 0)
-            {
-                std::cout << words[j];
-                printed = true;
-            }
-        }
-        if (!printed)
-        {
-            std::cout << i;
-        }
-        std::cout << '\n';
-    }
-
-}
-
-
+#include <vector>
 
 int main() {
-    FizzBuzz(150);
+    const std::vector<std::string_view> names {"Alex", "Betty", "Caroline", "Dave", "Emily", "Fred", "Greg", "Holly"};
+    
+    std::string user_input;
+    std::cout << "Enter a name to search for: ";
+    std::getline(std::cin, user_input);
+
+    for(const auto& name : names)
+    {
+        if(name == user_input) {
+            std::cout << "Found: " << name << '\n';
+            return 0;
+        }
+    }
+    std::cout << "Name not found." << '\n';
     return 0;
 }
