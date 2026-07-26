810a572aed5e9c7734281a316620c803abe2942f
oliveeah <olliebennettx@gmail.com>
2026-07-26 22:01:34 +0100

    quiz

--- main.cpp
+++ main.cpp
@@ -1,75 +1,12 @@
 #include <iostream>
 #include <vector>
-
-namespace {
-    enum class StackOp
-    {
-        Push,
-        Pop,
-        Peek
-    };
-}
-    
-template <typename T>
-auto printStack(StackOp op, const std::vector<T>& stack) -> void
-{   
-    switch(op)
-    {
-        case StackOp::Push:
-            std::cout << "Push: ";
-            break;
-        case StackOp::Pop:
-            std::cout << "Pop: ";
-            break;
-        case StackOp::Peek:
-            std::cout << "Peek: ";
-            break;
-    }
-
-    std::cout << "(Stack:";
-
-    if(stack.empty())
-    {
-        std::cout << " empty";
-    }
-    else
-    {
-        for(const auto& i : stack)
-        {
-            std::cout << ' ' << i;
-        }
-    }
-    std::cout << ")\n";
-}
-
-template <typename T>
-auto popStack(std::vector<T>& stack) -> void
-{
-    if(!stack.empty())
-    {
-        stack.pop_back();
-        printStack(StackOp::Pop, stack);
-    }
-}
-
-template <typename T>
-auto pushStack(std::vector<T>& stack, T& val) -> void
-{
-    stack.push_back(val);
-    printStack(StackOp::Push, stack);
-}
+#include <string_view>
 
 int main() {
-    std::vector<int> stack {};
 
-    pushStack(stack, 1);
-    pushStack(stack, 2);
-    pushStack(stack, 3);
-    popStack(stack);
-    pushStack(stack, 4);
-    popStack(stack);
-    popStack(stack);
-    popStack(stack);
+    std::vector evenNumbers { 2, 4, 6, 8, 10 };
+    const std::vector otherNumberss { 1.2, 3.4, 5.6, 7.8 };
+    const std::vector<std::string_view> names { "Alice", "Bob", "Charlie" };
 
     return 0;
 }
