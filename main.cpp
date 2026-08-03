29c036a1e68828d353b8a9e62f4fe1ec8be861df
oliveeah <olliebennettx@gmail.com>
2026-08-03 18:11:33 +0100

    new exercise first draft

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <vector>
+#include <utility>
+
+auto foo (const std::vector<int>& vec) -> std::pair<int, int> {
+    if(vec.empty())
+    {
+        return {0, 0};
+    }
+
+    const auto size { vec.size() };
+    int smallest { vec[0] }, largest { vec[0] };
+    for (auto i {0uz}; i < size; ++i) {
+        if (vec[i] < smallest) {
+            smallest = vec[i];
+        }
+        if (vec[i] > largest) {
+            largest = vec[i];
+        }
+    }
+    return {smallest, largest};
+
+}
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << std::pair<int,int>(foo({1, 2, 3, 4, 5})).first << std::endl;
+    std::cout << std::pair<int,int>(foo({1, 2, 3, 4, 5})).second << std::endl;
     return 0;
 }
