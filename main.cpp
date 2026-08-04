1bd69cdc19aa43bf7d350f8b3458b46554b9d8b2
oliveeah <olliebennettx@gmail.com>
2026-08-04 20:25:48 +0100

    array subscripting

--- main.cpp
+++ main.cpp
@@ -4,5 +4,10 @@
 int main()
 {
     constexpr std::array<char, 5> arr = {'H', 'e', 'l', 'l', 'o'};
+    std::cout << "Array length is: " << arr.size() << std::endl;
+    std::cout << "last element is: " << arr[4] << std::endl;
+    std::cout << "last element is: " << arr.at(4) << std::endl;
+    std::cout << "last element is: " << std::get<4>(arr) << std::endl;
+
     return 0;
 }
