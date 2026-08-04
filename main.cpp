6f1d1d1241f5e8abb956673eb14f90a194e84f4b
oliveeah <olliebennettx@gmail.com>
2026-08-04 16:45:49 +0100

    o2 -> o1

--- main.cpp
+++ main.cpp
@@ -2,25 +2,6 @@
 #include <vector>
 #include <utility>
 
-template <typename T>
-auto findMinMaxValues (const std::vector<T>& vec) -> std::pair<T, T> {
-    if(vec.empty()) {
-        throw std::invalid_argument("Vector is empty");
-    }
-
-    const auto size { vec.size() };
-    auto smallestValue { vec[0] }, largestValue { vec[0] };
-
-    for (auto i {1uz}; i < size; ++i) {
-        if (vec[i] < smallestValue) {
-            smallestValue = vec[i];
-        }
-        if (vec[i] > largestValue) {
-            largestValue = vec[i];
-        }
-    }
-    return {smallestValue, largestValue};
-}
 
 template <typename T>
 auto findMinMaxIndices (const std::vector<T>& vec) -> std::pair<std::size_t, std::size_t> {
@@ -28,17 +9,13 @@ auto findMinMaxIndices (const std::vector<T>& vec) -> std::pair<std::size_t, std
         throw std::invalid_argument("Vector is empty");
     }
 
-    const auto size { vec.size() };
-    auto smallestValue { vec[0] }, largestValue { vec[0] };
     auto smallestIndex { 0uz }, largestIndex { 0uz };
 
-    for (auto i {1uz}; i < size; ++i) {
-        if (vec[i] < smallestValue) {
-            smallestValue = vec[i];
+    for (auto i {1uz}; i < vec.size(); ++i) {
+        if (vec[i] < vec[smallestIndex]) {
             smallestIndex = i;
         }
-        if (vec[i] > largestValue) {
-            largestValue = vec[i];
+        if (vec[i] > vec[largestIndex]) {
             largestIndex = i;
         }
     }
@@ -46,22 +23,21 @@ auto findMinMaxIndices (const std::vector<T>& vec) -> std::pair<std::size_t, std
 }
 
 template <typename T>
-auto printMinMaxValuesAndIndices (const std::vector<T>& vec) -> void {
+auto printMinMaxInfo (const std::vector<T>& vec) -> void {
     const auto [minIndex, maxIndex] = findMinMaxIndices(vec);
-    const auto [minValue, maxValue] = findMinMaxValues(vec);
 
     std::cout << "With array (";
     for(const auto& value : vec) {
         std::cout << ' ' << value;
     }
     std::cout << " )\n";
-    std::cout << "The min element has index " << minIndex << " and value " << minValue << '\n';
-    std::cout << "The max element has index " << maxIndex << " and value " << maxValue << '\n';
+    std::cout << "The min element has index " << minIndex << " and value " << vec[minIndex] << '\n';
+    std::cout << "The max element has index " << maxIndex << " and value " << vec[maxIndex] << '\n';
 }
 
 
 int main() {
-    printMinMaxValuesAndIndices(std::vector{ 3, 8, 2, 5, 7, 8, 3 });
-    printMinMaxValuesAndIndices(std::vector{ 5.5, 2.7, 3.3, 7.6, 1.2, 8.8, 6.6 });
+    printMinMaxInfo(std::vector{ 3, 8, 2, 5, 7, 8, 3 });
+    printMinMaxInfo(std::vector{ 5.5, 2.7, 3.3, 7.6, 1.2, 8.8, 6.6 });
     return 0;
 }
