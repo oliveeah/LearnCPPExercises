bfb91e0e88520dd8b4a35ab4053a21d3a6896916
oliveeah <olliebennettx@gmail.com>
2026-08-04 21:00:28 +0100

    learncpp iterating thru array

--- main.cpp
+++ main.cpp
@@ -1,13 +1,32 @@
-#include <iostream>
 #include <array>
+#include <iostream>
+
+auto printComma(std::size_t length, std::size_t index) -> void
+{
+    (index == length - 1) ? std::cout << "" : std::cout << ", ";
+}
+template <typename T, std::size_t N>
+auto printArray(const std::array<T, N> &arr) -> void
+{
+    static_assert(N > 0, "Array must not be empty");
+
+    std::cout << "The array (";
+
+    for (auto i{0uz}; i < arr.size(); ++i)
+    {
+        std::cout << arr[i];
+        printComma(arr.size(), i);
+    }
+    std::cout << ") has " << "length " << arr.size() << '\n';
+}
 
 int main()
 {
-    constexpr std::array<char, 5> arr = {'H', 'e', 'l', 'l', 'o'};
-    std::cout << "Array length is: " << arr.size() << std::endl;
-    std::cout << "last element is: " << arr[4] << std::endl;
-    std::cout << "last element is: " << arr.at(4) << std::endl;
-    std::cout << "last element is: " << std::get<4>(arr) << std::endl;
+    constexpr std::array arr1{1, 4, 9, 16};
+    printArray(arr1);
+
+    constexpr std::array arr2{'h', 'e', 'l', 'l', 'o'};
+    printArray(arr2);
 
     return 0;
 }
