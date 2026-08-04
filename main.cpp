e8d358dddf2e4949d6a63afbbb5c8cdea06cd2a6
oliveeah <olliebennettx@gmail.com>
2026-08-04 21:06:57 +0100

    minor changes

--- main.cpp
+++ main.cpp
@@ -1,23 +1,20 @@
 #include <array>
 #include <iostream>
 
-auto printComma(std::size_t length, std::size_t index) -> void
-{
-    (index == length - 1) ? std::cout << "" : std::cout << ", ";
-}
 template <typename T, std::size_t N>
 auto printArray(const std::array<T, N> &arr) -> void
 {
-    static_assert(N > 0, "Array must not be empty");
-
     std::cout << "The array (";
-
-    for (auto i{0uz}; i < arr.size(); ++i)
+    const auto length{arr.size()};
+    for (auto i{0uz}; i < length; ++i)
     {
         std::cout << arr[i];
-        printComma(arr.size(), i);
+        if (i < length - 1)
+        {
+            std::cout << ", ";
+        }
     }
-    std::cout << ") has " << "length " << arr.size() << '\n';
+    std::cout << ") has " << "length " << length << '\n';
 }
 
 int main()
