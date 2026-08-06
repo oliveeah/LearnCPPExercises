49a9b0bcd8062c16314fc54532a92396dfcd3d44
oliveeah <olliebennettx@gmail.com>
2026-08-06 11:47:51 +0100

    traversing thru c style string

--- main.cpp
+++ main.cpp
@@ -1,69 +1,19 @@
 #include <iostream>
-#include <limits>
-#include <cmath>
+#include <cstring>
 
-namespace
+auto foo(const char *str) -> void
 {
-    constexpr int perfectSquares[]{0, 1, 4, 9};
-    constexpr int sentinel{-1};
-    constexpr int minPerfectSquare{perfectSquares[0]};
-    constexpr int maxPerfectSquare{perfectSquares[std::size(perfectSquares) - 1]};
-}
-
-auto isPerfectSquare(int num) -> bool
-{
-    for (int square : perfectSquares)
+    const char *start{str};
+    const char *end{str + std::strlen(str) - 1};
+    for (const char *p{start}; p <= end; ++p)
     {
-        if (num == square)
-        {
-            return true;
-        }
+        std::cout << *p;
     }
-    return false;
-}
-
-auto getNumberFromUser() -> int
-{
-    std::cout << "Enter a single digit integer, or -1 to quit: ";
-    int number{};
-    while (true)
-    {
-        std::cin >> number;
-
-        if (!std::cin)
-        {
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            std::cout << "Invalid input. Enter a single digit integer, or -1 to quit: ";
-            continue;
-        }
-        else if (number == sentinel || (number >= minPerfectSquare && number <= maxPerfectSquare))
-        {
-            return number;
-        }
-        std::cout << "Invalid input. Enter a single digit integer, or -1 to quit: ";
-    }
+    std::cout << std::endl;
 }
 
 int main()
 {
-    while (true)
-    {
-        int number{getNumberFromUser()};
-        if (number == sentinel)
-        {
-            std::cout << "Exiting the program." << std::endl;
-            break;
-        }
-
-        if (isPerfectSquare(number))
-        {
-            std::cout << number << " is a perfect square." << std::endl;
-        }
-        else
-        {
-            std::cout << number << " is not a perfect square." << std::endl;
-        }
-    }
+    foo("Hello, World!");
     return 0;
 }
