d7e6a0e3f726f6d14d0f22238cb8c15ea469d2aa
oliveeah <olliebennettx@gmail.com>
2026-08-27 20:47:36 +0000

    Update launch configuration and refactor main function to include Student struct and max_element usage

--- main.cpp
+++ main.cpp
@@ -2,25 +2,38 @@
 #include <array>
 #include <iostream>
 #include <string_view>
+#include <string>
+
+struct Student
+{
+  std::string name;
+  int points;
+};
 
 int main()
 {
-  // Print a value and count how many times @print has been called.
-  auto print{
-    [](auto value) {
-      static int callCount{ 0 };
-      std::cout << callCount++ << ": " << value << '\n';
-    }
-  };
+std::array<Student, 8> arr{
+  { { "Albert", 3 },
+    { "Ben", 5 },
+    { "Christine", 2 },
+    { "Dan", 8 }, 
+    { "Enchilada", 4 },
+    { "Francis", 1 },
+    { "Greg", 3 },
+    { "Hagrid", 5 } }
+};
 
-  print("hello"); // 0: hello
-  print("world"); // 1: world
+auto i { std::max_element(
+  arr.begin(),
+   arr.end(),
+   [](const Student& x, const Student& y)
+   {
+    return x.points < y.points;
+   }
+)};
 
-  print(1); // 0: 1
-  print(2); // 1: 2
+std::cout << "The student with the most points is: " << i->name << " with " << i->points << " points." << std::endl;
 
-  print("ding dong"); // 2: ding dong
-
-  return 0;
+return 0;
 }
 
