8fe67a5a4ead7f02f4769f03dd5ea52f54c25266
oliveeah <olliebennettx@gmail.com>
2026-08-27 20:56:20 +0000

    Refactor main function to replace Student struct with Season struct and implement sorting by average temperature

--- main.cpp
+++ main.cpp
@@ -2,38 +2,31 @@
 #include <array>
 #include <iostream>
 #include <string_view>
-#include <string>
 
-struct Student
+struct Season
 {
-  std::string name;
-  int points;
+  std::string_view name{};
+  double averageTemperature{};
 };
 
 int main()
 {
-std::array<Student, 8> arr{
-  { { "Albert", 3 },
-    { "Ben", 5 },
-    { "Christine", 2 },
-    { "Dan", 8 }, 
-    { "Enchilada", 4 },
-    { "Francis", 1 },
-    { "Greg", 3 },
-    { "Hagrid", 5 } }
-};
+  std::array<Season, 4> seasons{
+    { { "Spring", 285.0 },
+      { "Summer", 296.0 },
+      { "Fall", 288.0 },
+      { "Winter", 263.0 } }
+  };
 
-auto i { std::max_element(
-  arr.begin(),
-   arr.end(),
-   [](const auto& x, const auto& y)
-   {
-    return x.points < y.points;
-   }
-)};
+  std::sort(seasons.begin(), seasons.end(),[](const auto& x, const auto& y){
+    return x.averageTemperature < y.averageTemperature;
+  }
+  );
 
-std::cout << "The student with the most points is: " << i->name << " with " << i->points << " points." << std::endl;
+  for (const auto& season : seasons)
+  {
+    std::cout << season.name << '\n';
+  }
 
-return 0;
+  return 0;
 }
-
