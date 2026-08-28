54a259fec22d2c65a9a163b5af6bd571b6602c34
oliveeah <olliebennettx@gmail.com>
2026-08-28 10:17:25 +0100

    funtion pointer testing

--- main.cpp
+++ main.cpp
@@ -3,24 +3,31 @@
 #include <iostream>
 #include <string_view>
 
-int main()
+bool print()
 {
-  // Print a value and count how many times @print has been called.
-  auto print{
-    [](auto value) {
-      static int callCount{ 0 };
-      std::cout << callCount++ << ": " << value << '\n';
-    }
-  };
-
-  print("hello"); // 0: hello
-  print("world"); // 1: world
-
-  print(1); // 0: 1
-  print(2); // 1: 2
-
-  print("ding dong"); // 2: ding dong
-
-  return 0;
+    std::cout << "print called";
+    return true;
 }
 
+int main()
+{
+    // Print a value and count how many times @print has been called.
+    auto print_{
+        [](auto value)
+        {
+            static int callCount{0};
+            std::cout << callCount++ << ": " << value << '\n';
+        }};
+
+    print_("hello"); // 0: hello
+    print_("world"); // 1: world
+
+    bool (*fnPtr)(){print}; // Function pointer to the print function
+
+    print_(1); // 0: 1
+    print_(2); // 1: 2
+
+    print_("ding dong"); // 2: ding dong
+    fnPtr();             // Call the print function through the function pointer
+    return 0;
+}
