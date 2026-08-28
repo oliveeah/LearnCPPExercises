ca880cec0269a3e9db9acffdb2382bc08b2f45b4
oliveeah <olliebennettx@gmail.com>
2026-08-28 17:19:10 +0000

    removed stuff to implement IO overloads soon

--- main.cpp
+++ main.cpp
@@ -1,32 +1,6 @@
-#include <algorithm>
-#include <array>
 #include <iostream>
-#include <string_view>
 
-struct Season
-{
-  std::string_view name{};
-  double averageTemperature{};
-};
-
-int main()
-{
-  std::array<Season, 4> seasons{
-    { { "Spring", 285.0 },
-      { "Summer", 296.0 },
-      { "Fall", 288.0 },
-      { "Winter", 263.0 } }
-  };
-
-  std::sort(seasons.begin(), seasons.end(),[](const auto& x, const auto& y){
-    return x.averageTemperature < y.averageTemperature;
-  }
-  );
-
-  for (const auto& season : seasons)
-  {
-    std::cout << season.name << '\n';
-  }
-
-  return 0;
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
 }
