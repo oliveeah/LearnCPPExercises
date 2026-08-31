271019e140362e4bb83c0453d64ab70846176fc1
oliveeah <olliebennettx@gmail.com>
2026-08-31 14:02:46 +0100

    Implement Fraction class with print functionality and update main logic

--- main.cpp
+++ main.cpp
@@ -1,15 +1,37 @@
 #include <iostream>
-#include <string>
+#include <memory>
+
+class Fraction
+{
+private:
+    int m_numerator{0};
+    int m_denominator{1};
+
+public:
+    Fraction(int numerator = 0, int denominator = 1) : m_numerator{numerator}, m_denominator{denominator}
+    {
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const Fraction &f1)
+    {
+        out << f1.m_numerator << '/' << f1.m_denominator;
+        return out;
+    }
+};
+
+void printFraction(const Fraction *ptr)
+{
+    if (ptr)
+        std::cout << *ptr << '\n';
+    else
+        std::cout << "No fraction\n";
+}
 
 int main()
 {
-    const char *bar = "bar";
-    const char *bar = "bar";
-    std::string a = "a";
-    auto a1 = [a]()
-    {
-        std::string a = "aa";
-        return a;
-    };
-    std::cout << a1();
+    auto frac1{std::make_unique<Fraction>(3, 5)};
+
+    printFraction(frac1.get());
+
+    return 0;
 }
