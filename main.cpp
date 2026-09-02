1addcdb23595c31d966d41de1e4692cc3a02c03d
oliveeah <olliebennettx@gmail.com>
2026-09-02 13:48:42 +0100

    Refactor main.cpp: Remove ListNode structure and implement removeDuplicates function to eliminate duplicates from a sorted array

--- main.cpp
+++ main.cpp
@@ -1,71 +1,29 @@
-#include <iostream>
+
+#include <vector>
 using namespace std;
-struct ListNode
-{
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
+#include <iostream>
 
 class Solution
 {
 public:
-    ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
+    int removeDuplicates(vector<int> &nums)
     {
-        if (list1 == nullptr)
-            return list2;
-        if (list2 == nullptr)
-            return list1;
+        if (nums.empty())
+            return 0;
 
-        if (list2 == nullptr && list1 == nullptr)
-            return nullptr;
+        auto numsSize{nums.size()};
+        auto read_p{1uz};
+        auto write_p{1uz};
+        auto lastNum{nums[0]};
 
-        ListNode *head = new ListNode();
-        ListNode *current = head;
-        while (list1 != nullptr && list2 != nullptr)
-        {
-            if (list1->val < list2->val)
+        for (; read_p < numsSize; ++read_p)
+            if (nums[read_p] != lastNum)
             {
-                current->next = list1;
-                list1 = list1->next;
+                nums[write_p] = nums[read_p];
+                lastNum = nums[read_p];
+                ++write_p;
             }
-            else
-            {
-                current->next = list2;
-                list2 = list2->next;
-            }
-            current = current->next;
-        }
-        if (list1 != nullptr)
-        {
-            current->next = list1;
-        }
-        if (list2 != nullptr)
-        {
-            current->next = list2;
-        }
-        return head->next;
+
+        return write_p;
     }
 };
-
-int main()
-{
-    ListNode *list1 = new ListNode(1, new ListNode(3, new ListNode(5)));
-    ListNode *list2 = new ListNode(2, new ListNode(4, new ListNode(6)));
-
-    Solution solution;
-    ListNode *mergedList = solution.mergeTwoLists(list1, list2);
-
-    // Print the merged list
-    ListNode *current = mergedList;
-    while (current != nullptr)
-    {
-        cout << current->val << " ";
-        current = current->next;
-    }
-    cout << endl;
-
-    return 0;
-}
