a3656aa39408c8056281d7427d224030bb669122
oliveeah <olliebennettx@gmail.com>
2026-09-06 17:10:18 +0000

    leetcode 724

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int leftSum {},rightSum {};
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+        for (int num : nums) {
+            rightSum += num;
+        }
+        
+        for (auto i {0uz}; i < nums.size(); ++i) {
+            rightSum -= nums[i];
+            if(leftSum==rightSum)
+                return static_cast<int>(i);            
+            leftSum += nums[i];
+
+        }
+    return -1;
+    }
+};
+
+int main()
+{
+  Solution sol;
+  vector<int> nums = {1};
+  int pivot = sol.pivotIndex(nums);
+  cout << "Pivot index: " << pivot << endl;
 }
